c60ac9941b917ccafd886027ad04270272d6a3dc
clement.bramy <clement.bramy@accenture.com>
2023-02-07 17:14:41 +1100

    initial commit

---
+++ src/main/java/com/accenture/bootcamp/barkbuddies/BarkbuddiesApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.bootcamp.barkbuddies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarkbuddiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarkbuddiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/bootcamp/barkbuddies/BarkbuddiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.bootcamp.barkbuddies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarkbuddiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
