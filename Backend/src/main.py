e2d6ef32bafd750eccdd9755d5cdc124c859c3fd
joelabraham <z5310056@ad.unsw.edu.au>
2023-02-08 20:29:12 +1100

    Added comments for readability

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -20,6 +20,8 @@ CORS(APP)
 
 @APP.route("/register", methods=['POST'])
 def signup():
+    """Retrieving data from a post request and creating a dog to store in data"""
+    # Retrieving data from post request and creating a dog for that data to st
     payload = request.get_json()
     dog = Dog(payload["Name"], payload["age"], payload["suburb"], payload["breed"], payload["gender"], payload["photo"], payload["temperament"], payload["bio"], payload["email"], payload["name"])
     with open("data.json", "r+") as file:
@@ -31,6 +33,7 @@ def signup():
 
 @APP.route("/login", methods=['POST'])
 def login():
+    """Logging in a user given their email and validating it with database"""
     payload = request.get_json()
     with open("src/data.json", "r+") as file:
         file_data = load(file)
@@ -40,6 +43,7 @@ def login():
 
 @APP.route("/getalldogs", methods=["GET"])
 def getalldogs():
+        """Return all the dogs stored in the database"""
         with open("data.json", "r+") as file:
             file_data = load(file)
             all_dogs = file_data['dogs']
@@ -48,6 +52,7 @@ def getalldogs():
 
 @APP.route("/getallparks", methods=["GET"])
 def getallparks():
+        """Return all the parks stored in the database"""
         with open("data.json", "r+") as file:
             file_data = load(file)
             all_parks = file_data['parks']
